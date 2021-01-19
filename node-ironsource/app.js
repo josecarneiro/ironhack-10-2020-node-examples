8902ead23720a2f1811be6111d83c5c808150163
José Carneiro <josemcarneiro@gmail.com>
2021-01-19 18:57:46 +0000

    Adds scss styling and middleware

--- node-ironsource/app.js
+++ node-ironsource/app.js
@@ -1,15 +1,30 @@
 const express = require('express');
 const path = require('path');
-const resourceRouter = require('./routers/resource');
+const nodeSassMiddleware = require('node-sass-middleware');
+const morgan = require('morgan');
+const serveFavicon = require('serve-favicon');
 
+const resourceRouter = require('./routers/resource');
 const Resource = require('./models/resource');
-const { nextTick } = require('process');
 
 const app = express();
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
+app.use(serveFavicon(path.join(__dirname, 'public/favicon.ico')));
+app.use(
+  nodeSassMiddleware({
+    dest: path.join(__dirname, 'public/styles'),
+    src: path.join(__dirname, 'styles'),
+    force: true,
+    // outputStyle: 'compressed',
+    outputStyle: 'expanded',
+    prefix: '/styles'
+  })
+);
+app.use(express.static('public'));
+app.use(morgan('dev'));
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
